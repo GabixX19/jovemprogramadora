4644aa48fe08f2716b3ed42251fae6875f73ffbb
GabixX19 <sorvetefofo80@gmail.com>
2025-10-08 21:34:35 -0300

    aula 1

--- jovemProgramadoraJAVA/src/jovemprogramadorajava/JovemProgramadoraJAVA.java
+++ jovemProgramadoraJAVA/src/jovemprogramadorajava/JovemProgramadoraJAVA.java
@@ -5,7 +5,19 @@ public class JovemProgramadoraJAVA {
 
     public static void main(String[] args) {
   
+        String nome = "Gabrielle", Sobrenome = "Souza ";
+        
+        int idade = 19;
+        
+        int ano = 2025;
+        int anoNascimento = 2006;
+        
+        idade = ano - anoNascimento;
+                
         System.out.println("Ola Mundo");
+        System.out.println("Voce tem " + idade + " anos");
+        
+         
     }
     
 }
