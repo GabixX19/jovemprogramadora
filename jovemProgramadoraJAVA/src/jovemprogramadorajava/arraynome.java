71fd2287888a5b7180d15094f36c882a13d0bf56
GabixX19 <sorvetefofo80@gmail.com>
2025-10-21 21:39:24 -0300

    Aula array

---
+++ jovemProgramadoraJAVA/src/jovemprogramadorajava/arraynome.java
@@ -0,0 +1,17 @@
+
+package jovemprogramadorajava;
+
+
+public class arraynome {
+    
+    public static void main(String[] args) {
+        String nome;
+        String[] nomes = new String[3];
+        
+        nomes[0] = "Gabrielle";
+        nomes[1] = "Souza";
+        nomes[2] = "Faria";
+        
+        System.out.println("SEU NOME EH: " + nomes[0] + " " + nomes[1] + " " + nomes[2]);
+    }
+}
