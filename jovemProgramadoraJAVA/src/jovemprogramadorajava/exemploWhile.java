83aafb2e4d5fe886f407343e2febb2a59c525ee1
GabixX19 <sorvetefofo80@gmail.com>
2025-10-23 21:14:38 -0300

    aula while

---
+++ jovemProgramadoraJAVA/src/jovemprogramadorajava/exemploWhile.java
@@ -0,0 +1,34 @@
+
+package jovemprogramadorajava;
+
+import java.util.Scanner;
+
+
+public class exemploWhile {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int contador = 1;
+                
+                while (contador <= 10) {
+                    System.out.print(contador + " ");
+                    contador++;
+                }
+                System.out.println("\n");
+                
+                            contador = 1;
+                                    
+                             while (contador <= 100) {
+                    System.out.print(contador + " ");
+                    contador++;
+                             }
+                    System.out.println("\n");
+                    
+                    contador = 1;
+                    
+                     while (contador <= 1000) {
+                    System.out.print(contador + " ");
+                    contador++;
+                }
+    }
+}
---
+++ jovemProgramadoraJAVA/src/jovemprogramadorajava/tabuada.java
@@ -0,0 +1,18 @@
+
+package jovemprogramadorajava;
+
+import java.util.Scanner;
+
+
+public class tabuada {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numero;
+        System.out.println("DIGITE O NUMERO QUE VC DESEJA MONTAR A TABUADA");
+        numero = scan.nextInt();
+        
+        for(int i = 1; i <= 10; i++){
+            System.out.println(numero * i);
+        }
+    }
+}
